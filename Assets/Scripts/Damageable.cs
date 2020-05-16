93495aec714622569b384d7ff888226f22d5c4c9
Jonata Novais Cirqueira <Jonatanc0511@gmail.com>
2020-05-16 01:05:43 -0300

    Fazer player piscar ao tomar dano

--- Assets/Scripts/Damageable.cs
+++ Assets/Scripts/Damageable.cs
@@ -11,27 +11,36 @@ public class Damageable : MonoBehaviour {
 	public MonoBehaviour movementScript;
 	public float invulnerableDuration;
 	public Collider2D thisCollider;
+	public int blinkTimes;
+	public float blinkDuration;
+
 
 	private LayerMask playerLayer;
 	private LayerMask enemiesLayer;
 	private Animator anim;
 	private Rigidbody2D rb;
+	private SpriteRenderer renderer;
 	private float dazedRemainingTime;
 	private float intangibleRemainingTime;
+	private bool isDazed;
 
 	// Start is called before the first frame update
 	void Start() {
 		rb = GetComponent<Rigidbody2D>();
 		anim = GetComponent<Animator>();
+		renderer = GetComponent<SpriteRenderer>();
 		playerLayer = LayerMask.NameToLayer("Player");
 		enemiesLayer = LayerMask.NameToLayer("Enemies");
+		isDazed = false;
 	}
 
 	// Update is called once per frame
 	void Update() {
-		if (dazedRemainingTime <= 0) {
-			if (movementScript && !movementScript.enabled)
+		if (dazedRemainingTime <= 0 && isDazed) {
+			if (movementScript && !movementScript.enabled && isDazed) {
 				movementScript.enabled = true;
+				isDazed = false;
+			}
 		} else {
 			dazedRemainingTime -= Time.deltaTime;
 		}
@@ -41,6 +50,7 @@ public class Damageable : MonoBehaviour {
 		if (movementScript) {
 			movementScript.enabled = false;
 			dazedRemainingTime = dazedTime;
+			isDazed = true;
 			StartCoroutine(MakeInvulnerable(invulnerableDuration));
 		}
 
@@ -55,8 +65,18 @@ public class Damageable : MonoBehaviour {
 
 	private IEnumerator MakeInvulnerable(float time) {
 		Physics2D.IgnoreLayerCollision(playerLayer, enemiesLayer, true);
+		StartCoroutine(Blink());
 		yield return new WaitForSeconds(time);
 		Physics2D.IgnoreLayerCollision(playerLayer, enemiesLayer, false);
+	}
+ 
+	private IEnumerator Blink() {
+		for (int i = 0; i < blinkTimes; i++) {
+			renderer.enabled = false;
+			yield return new WaitForSeconds(blinkDuration);
+			renderer.enabled = true;
+			yield return new WaitForSeconds(blinkDuration*2);
+		}
 	}
 
 	private IEnumerator Die(Collider2D collider) {
