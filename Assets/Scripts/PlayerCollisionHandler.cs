34043dd96d8fd466cfa5bb7c4dd125520ea8fe81
Jonata Novais Cirqueira <Jonatanc0511@gmail.com>
2020-05-20 19:43:30 -0300

    Generalizar mais as colisões do player

--- Assets/Scripts/PlayerCollisionHandler.cs
+++ Assets/Scripts/PlayerCollisionHandler.cs
@@ -5,23 +5,30 @@ using UnityEngine;
 public class PlayerCollisionHandler : MonoBehaviour {
 
 	public int damage;
+	public Vector2 defaultKnockback;
 
-	private Damageable dmg;
+	private Damageable damageScript;
 	private SpriteRenderer sr;
 
 	void Start() {
-		dmg = GetComponent<Damageable>();
+		damageScript = GetComponent<Damageable>();
 		sr = GetComponent<SpriteRenderer>();
 	}
 
-	private void OnTriggerEnter2D(Collider2D collision) {
-		if (collision.gameObject.CompareTag("Enemy")) {
+	private void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.gameObject.CompareTag("Projectile")) {
 			ContactPoint2D[] points = new ContactPoint2D[6];
-			collision.GetContacts(points);
+			collider.GetContacts(points);
 			float xDirection = points[0].normal.x;
 			if (xDirection == 0f)
 				xDirection = sr.flipX ? 1f : -1f;
-			dmg.TakeDamage(damage, /*(temp)*/ new Vector2(xDirection, 2f), collision);
+			damageScript.TakeDamage(damage, new Vector2(xDirection * defaultKnockback.x, defaultKnockback.y), collider);
+
+		} else if (collider.gameObject.CompareTag("Enemy")) {
+			float horDist = gameObject.transform.position.x - transform.position.x;
+			Vector2 knockback = new Vector2(horDist > 0 ? defaultKnockback.x : -defaultKnockback.x, defaultKnockback.y);
+			damageScript.TakeDamage(damage, knockback, collider.gameObject.GetComponent<BoxCollider2D>());
 		}
 	}
+
 }
