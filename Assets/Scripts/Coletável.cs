2d418349230f643bd4ba473a49a95eb92002220f
Nikolas <nikolasVictorTola@gmail.com>
2020-05-25 16:11:52 -0300

    Adicionar papel coletável

---
+++ Assets/Scripts/Coletável.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coletável : MonoBehaviour
+{
+	private BoxCollider2D hitbox;
+	private int counter = 0;
+
+	public int total;
+
+	
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		hitbox = gameObject.GetComponent<BoxCollider2D>();
+		total = 4;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerEnter2D(Collider2D collision){
+		if (collision.gameObject.CompareTag("Player")){
+			Destroy(gameObject, 0.001f);
+		}
+	}
+}
