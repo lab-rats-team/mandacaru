fb1374560c85252e3ac96bf6f4f3adb77b5561e8
Jônata <Jonatanc0511@gmail.com>
2020-06-11 20:25:07 -0300

    Implementar sistema básico de pop-ups

--- Assets/Scripts/MenuService.cs
+++ Assets/Scripts/MenuService.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class MenuService : MonoBehaviour {
 
+	public GameObject[] popUps;
+
+	public void openPopUp(int popUpIdx) {
+		closeAllPopUps();
+		popUps[popUpIdx].SetActive(true);
+	}
+
     public void quit() => Application.Quit();
+
+	private void closeAllPopUps() {
+		foreach (GameObject popUp in popUps) {
+			popUp.SetActive(false);
+		}
+	}
 
 }
