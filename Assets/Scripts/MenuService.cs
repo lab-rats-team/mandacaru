4d569052382ca86ba0f94a722cc425662b0e46ac
Jônata <Jonatanc0511@gmail.com>
2020-06-11 23:45:41 -0300

    Organizar UI e fazer pop-ups funcionais

--- Assets/Scripts/MenuService.cs
+++ Assets/Scripts/MenuService.cs
@@ -1,19 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 
 public class MenuService : MonoBehaviour {
 
 	public GameObject[] popUps;
 
-	public void openPopUp(int popUpIdx) {
-		closeAllPopUps();
-		popUps[popUpIdx].SetActive(true);
+	public void Start() {
+		CloseAllPopUps();
 	}
 
-    public void quit() => Application.Quit();
+	public void OpenPopUp(int popUpIdx) {
+		CloseAllPopUps();
+		try {
+			popUps[popUpIdx].SetActive(true);
+		} catch (IndexOutOfRangeException e) {
+			Debug.Log("Falha ao tentar abrir pop-up com index inválido");
+		}
+	}
 
-	private void closeAllPopUps() {
+    public void Quit() => Application.Quit();
+
+	private void CloseAllPopUps() {
 		foreach (GameObject popUp in popUps) {
 			popUp.SetActive(false);
 		}
