f7d3a5e00694dc66b59aa462e3553c6c688bcfb0
Jônata <Jonatanc0511@gmail.com>
2020-06-12 22:07:45 -0300

    Implementar pop-up de configurações

--- Assets/Scripts/MenuService.cs
+++ Assets/Scripts/MenuService.cs
@@ -3,10 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuService : MonoBehaviour {
 
 	public GameObject[] popUps;
+
+	public GameObject ptButton;
+	public GameObject enButton;
+	public GameObject ptCircle;
+	public GameObject enCircle;
+
+	public GameObject fsButton;
+	public GameObject winButton;
+	public GameObject fsCircle;
+	public GameObject winCircle;
+
+	public Sprite emptyCircle;
+	public Sprite filledCircle;
 
 	public void Start() {
 		CloseAllPopUps();
@@ -27,6 +41,18 @@ public class MenuService : MonoBehaviour {
 		foreach (GameObject popUp in popUps) {
 			popUp.SetActive(false);
 		}
+	}
+
+	public void setEnglish(bool english) {
+		// chamar tradutor
+		ptCircle.GetComponent<Image>().sprite =  english ? emptyCircle : filledCircle;
+		enCircle.GetComponent<Image>().sprite = !english ? emptyCircle : filledCircle;
+	}
+
+	public void setFullscreen(bool fullscreen) {
+		// alterar janela
+		winCircle.GetComponent<Image>().sprite = fullscreen ? emptyCircle : filledCircle;
+		fsCircle.GetComponent<Image>().sprite = !fullscreen ? emptyCircle : filledCircle;
 	}
 
 }
