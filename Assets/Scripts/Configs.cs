40fd996b35aa1ba2374472b0efebefefe0bc8b94
Jônata <Jonatanc0511@gmail.com>
2020-06-13 10:58:03 -0300

    Implementar "salvar" e "cancelar" mudanças das configurações

---
+++ Assets/Scripts/Configs.cs
@@ -0,0 +1,47 @@
+using System;
+
+public class Configs {
+	private bool english, fullscreen;
+	private float musicVol, sfxVol;
+
+	public Configs() {}
+
+	public Configs(bool en, bool fs, float msc, float sfx) {
+		english = en;
+		fullscreen = fs;
+		musicVol = msc;
+		sfxVol = sfx;
+	}
+
+	public bool GetEnglish() {
+		return english;
+	}
+
+	public void SetEnglish(bool en) {
+		english = en;
+	}
+
+	public bool GetFullscreen() {
+		return fullscreen;
+	}
+
+	public void SetFullscreen(bool fs) {
+		fullscreen = fs;
+	}
+
+	public float GetMusicVol() {
+		return musicVol;
+	}
+
+	public void SetMusicVol(float msc) {
+		musicVol = msc;
+	}
+
+	public float GetSfxVol() {
+		return sfxVol;
+	}
+
+	public void SetSfxVol(float sfx) {
+		sfxVol = sfx;
+	}
+}
--- Assets/Scripts/MenuService.cs
+++ Assets/Scripts/MenuService.cs
@@ -22,8 +22,15 @@ public class MenuService : MonoBehaviour {
 	public Sprite emptyCircle;
 	public Sprite filledCircle;
 
+	public Slider mscSlider;
+	public Slider sfxSlider;
+
+	private Configs currentConfigs;
+
 	public void Start() {
 		CloseAllPopUps();
+		currentConfigs = new Configs(false, true, 0.5f, 0.5f); // temporário
+		UpdateConfigUI();
 	}
 
 	public void OpenPopUp(int popUpIdx) {
@@ -43,16 +50,37 @@ public class MenuService : MonoBehaviour {
 		}
 	}
 
-	public void setEnglish(bool english) {
+	public void SetEnglishUI(bool english) {
 		// chamar tradutor
 		ptCircle.GetComponent<Image>().sprite =  english ? emptyCircle : filledCircle;
 		enCircle.GetComponent<Image>().sprite = !english ? emptyCircle : filledCircle;
 	}
 
-	public void setFullscreen(bool fullscreen) {
-		// alterar janela
+	public void SetFullscreenUI(bool fullscreen) {
 		winCircle.GetComponent<Image>().sprite = fullscreen ? emptyCircle : filledCircle;
 		fsCircle.GetComponent<Image>().sprite = !fullscreen ? emptyCircle : filledCircle;
+	}
+
+	public Configs GetConfigsFromUI() {
+		Configs cfg = new Configs();
+		cfg.SetEnglish(enCircle.GetComponent<Image>().sprite == filledCircle);
+		cfg.SetFullscreen(fsCircle.GetComponent<Image>().sprite == filledCircle);
+		cfg.SetMusicVol(mscSlider.value);
+		cfg.SetSfxVol(sfxSlider.value);
+		return cfg;
+	}
+
+	public void UpdateConfigUI() {
+		SetEnglishUI(currentConfigs.GetEnglish());
+		SetFullscreenUI(currentConfigs.GetFullscreen());
+		mscSlider.value = currentConfigs.GetMusicVol();
+		sfxSlider.value = currentConfigs.GetSfxVol();
+	}
+
+	public void SaveConfigs() {
+		Configs cfg = GetConfigsFromUI();
+		// setar janela e volumes
+		currentConfigs = cfg;
 	}
 
 }
