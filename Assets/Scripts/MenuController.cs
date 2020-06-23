3bda9e4fd98ed8722a10825a91eab7a4d8e85ef6
Jonata Novais Cirqueira <Jonatanc0511@gmail.com>
2020-06-23 16:36:13 -0300

    Usar PlayerPrefs para salvar preferências no disco rígido

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -12,22 +12,25 @@ public class MenuController : MonoBehaviour {
 		view.CloseAllPopUps();
 		currentConfigs = new ConfigsModel(false, true, 0.5f, 0.5f); // temporário
 		view.UpdateConfigs(currentConfigs);
+		ApplyFullScreen(PlayerPrefs.GetInt("fullscreen") == 1);
 	}
 
 	public void SaveConfigs() {
 		ConfigsModel cfg = view.GetConfigs();
 		ApplyFullScreen(cfg.GetFullscreen());
+		PlayerPrefs.SetInt("fullscreen", cfg.GetFullscreen() ? 1 : 0);
 		// setar volumes e idioma
 		currentConfigs = cfg;
+		PlayerPrefs.Save();
 	}
 
 	public void Quit() => Application.Quit();
 	
 	private void ApplyFullScreen(bool fullScreen) {
 		Resolution maxResol = Screen.resolutions[Screen.resolutions.Length - 1];
-		if(fullScreen)
+		if (fullScreen)
 			Screen.SetResolution(maxResol.width, maxResol.height, true);
 		else
-			Screen.SetResolution((int)Mathf.Round(maxResol.width/2), (int)Mathf.Round(maxResol.height/2), false);
+			Screen.SetResolution((int)Mathf.Round(maxResol.width / 2), (int)Mathf.Round(maxResol.height / 2), false);
 	}
 }
