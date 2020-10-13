902077ffcdc4cc32142159b0e464020b68edcaa0
Jonata Novais Cirqueira <Jonatanc0511@gmail.com>
2020-10-13 11:46:30 -0300

    Fazer créditos rolarem por script

---
+++ Assets/Scripts/CreditsManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsManager : MonoBehaviour {
+	public GameObject credits;
+	public float initialY;
+	public float finalY;
+	public float rollTime;
+	
+	private float currentY;
+
+    void Update() {
+		float interpolation = Time.time/rollTime;
+		currentY = initialY + (finalY - initialY) * interpolation;
+		
+		Vector3 pos = credits.transform.position;
+        credits.transform.position = new Vector3(pos.x, currentY, pos.z);
+    }
+}
