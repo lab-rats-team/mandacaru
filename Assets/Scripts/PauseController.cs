bb02bf50d5d33cd9859ac7e1cab8f3ace8810b48
Jonata Novais Cirqueira <Jonatanc0511@gmail.com>
2020-10-15 19:24:05 -0300

    Fazer configurações funcionarem

--- Assets/Scripts/PauseController.cs
+++ Assets/Scripts/PauseController.cs
@@ -2,10 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PauseController : MonoBehaviour {
 	
 	public GameObject[] popUps;
+
+	public GameObject ptButton;
+	public GameObject enButton;
+	public GameObject ptCircle;
+	public GameObject enCircle;
+
+	public GameObject fsButton;
+	public GameObject winButton;
+	public GameObject fsCircle;
+	public GameObject winCircle;
+
+	public Sprite emptyCircle;
+	public Sprite filledCircle;
+
+	public Slider mscSlider;
+	public Slider sfxSlider;
+	
+	private ConfigsModel currentConfigs;
+	
+	void Awake() {
+		bool english = PlayerPrefs.GetInt("english", 0) == 0 ? false : true;
+		bool fullscreen = PlayerPrefs.GetInt("fullscreen", 1) == 0 ? false : true;
+		float music = PlayerPrefs.GetFloat("musicVol", 1f);
+		float sfx = PlayerPrefs.GetFloat("sfxVol", 1f);
+		currentConfigs = new ConfigsModel(english, fullscreen, music, sfx);
+		
+		UpdateConfigs(currentConfigs);
+	}
 
     void Update() {
         if (Input.GetKeyDown(KeyCode.Escape)) {
@@ -24,10 +53,59 @@ public class PauseController : MonoBehaviour {
 		CloseAllPopUps();
 		Time.timeScale = 1f;
 	}
+
+	public void SaveConfigs() {
+		ConfigsModel cfg = GetConfigs();
+		ApplyFullScreen(cfg.GetFullscreen());
+		AudioManager.instance.UpdateSoundsVolume(cfg.GetMusicVol(), cfg.GetSfxVol());
+		LanguageManager.instance.LoadLanguage(cfg.GetEnglish() ? "en" : "pt");
+
+		PlayerPrefs.SetInt("fullscreen", cfg.GetFullscreen() ? 1 : 0);
+		PlayerPrefs.SetFloat("musicVol", cfg.GetMusicVol());
+		PlayerPrefs.SetFloat("sfxVol", cfg.GetSfxVol());
+		PlayerPrefs.SetInt("english", cfg.GetEnglish() ? 1 : 0);
+		PlayerPrefs.Save();
+
+		currentConfigs = cfg;
+	}
+
+	public ConfigsModel GetConfigs() {
+		ConfigsModel cfg = new ConfigsModel();
+		cfg.SetEnglish(enCircle.GetComponent<Image>().sprite == filledCircle);
+		cfg.SetFullscreen(fsCircle.GetComponent<Image>().sprite == filledCircle);
+		cfg.SetMusicVol(mscSlider.value);
+		cfg.SetSfxVol(sfxSlider.value);
+		return cfg;
+	}
+
+	public void SetEnglishUI(bool english) {
+		ptCircle.GetComponent<Image>().sprite =  english ? emptyCircle : filledCircle;
+		enCircle.GetComponent<Image>().sprite = !english ? emptyCircle : filledCircle;
+	}
+
+	public void SetFullscreenUI(bool fullscreen) {
+		winCircle.GetComponent<Image>().sprite = fullscreen ? emptyCircle : filledCircle;
+		fsCircle.GetComponent<Image>().sprite = !fullscreen ? emptyCircle : filledCircle;
+	}
+
+	public void UpdateConfigs(ConfigsModel cfg) {
+		SetEnglishUI(cfg.GetEnglish());
+		SetFullscreenUI(cfg.GetFullscreen());
+		mscSlider.value = cfg.GetMusicVol();
+		sfxSlider.value = cfg.GetSfxVol();
+	}
 	
 	public void Exit() => Application.Quit();
 	
 	public void ReturnToMainMenu() => SceneManager.LoadScene(0);
+	
+	private void ApplyFullScreen(bool fullScreen) {
+		Resolution maxResol = Screen.resolutions[Screen.resolutions.Length - 1];
+		if (fullScreen)
+			Screen.SetResolution(maxResol.width, maxResol.height, true);
+		else
+			Screen.SetResolution((int)Mathf.Round(maxResol.width / 1.7f), (int)Mathf.Round(maxResol.height / 1.7f), false);
+	}
 	
 	public void OpenPopUp(string popUpName) {
 		CloseAllPopUps();
