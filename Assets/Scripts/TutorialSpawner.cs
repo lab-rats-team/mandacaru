f2e51f783f8820998eea835da2ac651336b64034
Guilherme Yago <guiyago2015@gmail.com>
2020-11-13 17:31:40 -0300

    update tutorials

--- Assets/Scripts/TutorialSpawner.cs
+++ Assets/Scripts/TutorialSpawner.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TutorialSpawner : MonoBehaviour
-{
-   public GameObject tutorialPrefab;
+public class TutorialSpawner : MonoBehaviour {
+
+	public GameObject inimigoPrefab;
+	public float distanciaGeracao = 10.2f;
 	public float distanciaMaxima = 15.2f;
 
 	private GameObject tempPrefab;
 	private Transform player;
 	private Transform transf;
-	//private bool destroyed = true;
+	private bool destroyed = true;
 
 	// Start is called before the first frame update
 	void Start() {
@@ -21,17 +22,17 @@ public class TutorialSpawner : MonoBehaviour
 	// Update is called once per frame
 	void Update(){
 
-		
+		if (Vector3.Distance(transf.position, player.position) < distanciaGeracao && destroyed) {
 			tempPrefab = Instantiate (inimigoPrefab) as GameObject;
 			tempPrefab.transform.parent = transf;
 			tempPrefab.transform.position = new Vector3(transf.position.x, transf.position.y, transf.position.z);
-			//destroyed = false;
-		
+			destroyed = false;
+		}
 
 		if(tempPrefab) {
-			if (Vector3.Distance(tempPrefab.transform.position, player.position) >= distanciaMaxima) {
+			if (Vector3.Distance(tempPrefab.transform.position, player.position) >= distanciaMaxima && !destroyed) {
 				Destroy(tempPrefab);
-				//destroyed = true;
+				destroyed = true;
 			}
 		}
 	}
