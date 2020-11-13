3542247e5a47e94c9f57eef44817f8d36193c8c1
Guilherme Yago <guiyago2015@gmail.com>
2020-11-13 01:27:06 -0300

    iniciando scripts dos tutoriais

--- Assets/Scripts/TutorialSpawner.cs
+++ Assets/Scripts/TutorialSpawner.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class TutorialSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   public GameObject tutorialPrefab;
+	public float distanciaMaxima = 15.2f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private GameObject tempPrefab;
+	private Transform player;
+	private Transform transf;
+	//private bool destroyed = true;
+
+	// Start is called before the first frame update
+	void Start() {
+		player = GameObject.FindWithTag("Player").transform;
+		transf = GetComponent<Transform>();
+	}
+
+	// Update is called once per frame
+	void Update(){
+
+		
+			tempPrefab = Instantiate (inimigoPrefab) as GameObject;
+			tempPrefab.transform.parent = transf;
+			tempPrefab.transform.position = new Vector3(transf.position.x, transf.position.y, transf.position.z);
+			//destroyed = false;
+		
+
+		if(tempPrefab) {
+			if (Vector3.Distance(tempPrefab.transform.position, player.position) >= distanciaMaxima) {
+				Destroy(tempPrefab);
+				//destroyed = true;
+			}
+		}
+	}
 }
