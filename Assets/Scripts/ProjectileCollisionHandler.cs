998af7292c86ee0b722bfccfb96b801dc48b5b55
Jônata <Jonatanc0511@gmail.com>
2021-01-27 20:59:44 -0300

    Mudar comportamento do projétil

--- Assets/Scripts/ProjectileCollisionHandler.cs
+++ Assets/Scripts/ProjectileCollisionHandler.cs
@@ -6,15 +6,18 @@ public class ProjectileCollisionHandler : MonoBehaviour {
 
 	public bool towardsLeft;
 	public string[] collideWithTags;
+	public Vector2 knockback;
 
 	private void OnTriggerEnter2D(Collider2D collision) {
-		Damageable d = collision.gameObject.GetComponent<Damageable>();
-		if (d != null)
-			d.TakeDamage(1, new Vector2(0f, 3f), gameObject.GetComponent<CircleCollider2D>());
 
 		foreach (string tag in collideWithTags) {
-			if (collision.CompareTag(tag))
+			if (collision.CompareTag(tag)) {
+				Damageable d = collision.gameObject.GetComponent<Damageable>();
+				if (d != null) {
+					d.TakeDamage(1, knockback, gameObject.GetComponent<CircleCollider2D>());
+				}
 				Destroy(gameObject);
+			}
 		}
 	}
 
