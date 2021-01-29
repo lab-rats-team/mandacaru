daa61910168402f5670cad31d7ef0017c5978a01
Jônata <Jonatanc0511@gmail.com>
2021-01-29 13:12:30 -0300

    Impedir player de sair do level mais cedo

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -8,18 +8,24 @@ public class LevelExit : MonoBehaviour {
 	public GameObject player;
 	public GameObject tutoPrefab;
 
+	private bool allowed;
 	private GameObject tutoInstance;
+
+	void Start() {
+		allowed = false;
+	}
 
     // Update is called once per frame
     void OnTriggerEnter2D(Collider2D other) {
 		tutoInstance = Instantiate(tutoPrefab) as GameObject;
 		tutoInstance.transform.position = transform.position;
-		Debug.Log(tutoInstance.transform.position + " " + transform.position);
+		allowed = true;
     }
 	
 	void Update() {
 		if (Input.GetKeyDown(KeyCode.W) 
-					&& Mathf.Abs(transform.position.x - player.transform.position.x) < .6f) {
+					&& Mathf.Abs(transform.position.x - player.transform.position.x) < .6f
+					&& allowed) {
 				SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 		}
 	}
